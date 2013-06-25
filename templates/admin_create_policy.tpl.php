ca625d50725c0c58fa72462597c72af9f7d747ba
avel <avel@noc.uoa.gr>
2013-06-25 16:02:58 +0300

    Small beautification fix for the No policies defined screen

--- templates/admin_create_policy.tpl.php
+++ templates/admin_create_policy.tpl.php
@@ -1,11 +1,13 @@
 
+
 <div class="row">
-<div class="six centered columns">
+<div class="span8 offset2">
 
 <form name="policy_new_form" action="admin_set_policies.php" method="POST">
-    <p><?= _("There are no defined password policies on the LDAP Server.")?></p>
+    <h2><?= _("There are no defined password policies on the LDAP Server.")?></h2>
     <p><?= _("Create a new policy with some predefined values:")?></p>
-    <input type="submit" class="nice medium radius blue button" value="<?= _("Create New Policy") ?>" name="policy_create_default" />
+    
+    <input type="submit" class="btn btn-primary" value="<?= _("Create New Policy") ?>" name="policy_create_default" />
 </form>
 
 </div>
