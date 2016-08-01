6eb3d0055bfdac2189fdbef2110e6f8dbaafa5a4
trougakoss <trougakoss@noc.uoa.gr>
2016-08-01 13:51:13 +0300

    Allowed ips to serve reset pass new ws

---
+++ config/access.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return array(
+        array('ip' => '195.134.100.11', 'key' => '895b91d078b6d4f925b5fd7064d1f23d', 'institution' => 'idm'),
+        array('ip' => '195.134.100.11', 'key' => '5b1cab5e7f280cce818aa2ebfaee2213', 'institution' => 'all'),
+        array('ip' => '127.0.0.1', 'key' => 'c3356c0d790f705ccab4b3fd98279415', 'institution' => 'me'),
+);
+
+
+?>
